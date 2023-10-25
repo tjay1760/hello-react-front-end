1114f792ef4a57ca691b9f7335f9719bae58dc3e
tjay1760 <mcjthiongo@gmail.com>
2023-10-26 01:07:57 +0300

    Fix file path errors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Greeting from './greeting';
 
-export const App = () => {
+function App() {
   return (
-    <div>App</div>
-  )
+    <div className="App">
+      <Routes>
+        <Route exact path="/" element={<Greeting />} />
+      </Routes>
+    </div>
+  );
 }
+
+export default App;
